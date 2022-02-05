18029e18d3efa76fb774720b9e355c3f11b9d625
munib <munibkhatri123@gmail.com>
2022-02-05 12:10:41 +0500

    integrate contact from

--- src/components/Home/ContactUs.jsx
+++ src/components/Home/ContactUs.jsx
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Container, Form, Button, Spinner } from "react-bootstrap";
 import { HiOutlineLocationMarker } from "react-icons/hi";
-import { FaLinkedinIn, FaGooglePlusG, FaFacebookF, FaTwitter } from "react-icons/fa";
 import {
-  FiPhoneCall,
-  FiMail,
-} from "react-icons/fi";
+  FaLinkedinIn,
+  FaGooglePlusG,
+  FaFacebookF,
+  FaTwitter,
+} from "react-icons/fa";
+import { FiPhoneCall, FiMail } from "react-icons/fi";
 import Scroll from "react-scroll";
 import Action from "../../middleware/API";
 import baseURL from "../../middleware/BaseURL";
+import { useHistory } from "react-router-dom";
 
 const ContactUs = () => {
+  let history = useHistory();
+
   const [alert, setAlert] = useState("");
   const [spin, setSpin] = useState("Send Message");
   const [contact, setcontact] = useState([]);
@@ -24,22 +29,22 @@ const ContactUs = () => {
   });
   const getcontact = async () => {
     const response = await Action.get("/setting", {});
-    console.log(response)
+    console.log(response);
     if (response.data.success == true) {
-
-      setcontact(response.data.data);
+      console.log(response.data.data);
+      setcontact(response.data.data[0]);
     } else {
     }
   };
   const postquery = async () => {
-
     const response = await Action.post("/query", data, {});
 
-    // if (response.data.success == true) {
-    // } else {
-    // }
+    if (response.data.success == true) {
+      return setAlert(response.data.message);
+    } else {
+      return setAlert(response.data.message);
+    }
   };
-
 
   useEffect(async () => {
     await getcontact();
@@ -52,76 +57,66 @@ const ContactUs = () => {
           <h3>Contact Us</h3>
           <p>We will get to you asap!</p>
           <Row className="contact-form mt-5">
-
             <Col md="4" sm="12" className=" text-dark  contact-details">
-
               <Row className="each-detail">
                 <Col xs="2">
                   <span className="mb-1">
-                    <FiPhoneCall size={ 45 } />
+                    <FiPhoneCall size={45} />
                   </span>
                 </Col>
                 <Col xs="9">
-                  <h6>Call us</h6>
-                  <b className="">
-                    { contact.Phone }
-                  </b>
+                  <h6>{contact.Phone}</h6>
                 </Col>
-
               </Row>
               <Row className="each-detail">
                 <Col xs="2">
                   <span className="mb-1">
-                    <FiMail size={ 45 } />
+                    <FiMail size={45} />
                   </span>
                 </Col>
                 <Col xs="9">
-                  <h6>Email us</h6>
-                  <b className="">
-                    { contact.Email }
-                  </b>
+                  <h6>{contact.Email}</h6>
                 </Col>
-
               </Row>
               <Row className="each-detail">
                 <Col xs="2">
                   <span className="mb-1">
-                    <HiOutlineLocationMarker size={ 45 } />
+                    <HiOutlineLocationMarker size={45} />
                   </span>
                 </Col>
                 <Col xs="9">
-                  <h6>Address</h6>
-                  <b className="">
-                    { contact.Address }
-                  </b>
+                  <h6>{contact.Address}</h6>
                 </Col>
-
               </Row>
               <div className="contact-footer-icon d-flex">
-                <a href={ contact.fb } target="_blank">
+                <a href={contact.fb} target="_blank">
                   <span className="bg-primary">
-                    <FaFacebookF size={ 25 } />
+                    <FaFacebookF size={25} />
                   </span>
                 </a>
-                <a href={ contact.twitter } target="_blank">
+                <a href={contact.twitter} target="_blank">
                   <span className="bg-info">
-                    <FaTwitter size={ 25 } />
+                    <FaTwitter size={25} />
                   </span>
                 </a>
-                <a href={ contact.google } target="_blank">
+                <a href={contact.google} target="_blank">
                   <span className="bg-danger">
-                    <FaGooglePlusG size={ 25 } />
+                    <FaGooglePlusG size={25} />
                   </span>
                 </a>
-                <a href={ contact.linkedin } target="_blank">
+                <a href={contact.linkedin} target="_blank">
                   <span className="bg-dark">
-                    <FaLinkedinIn size={ 25 } />
+                    <FaLinkedinIn size={25} />
                   </span>
                 </a>
               </div>
             </Col>
 
-            <Col xs="12" md={ { offset: 2, span: 6 } } className="contact-inputs text-right">
+            <Col
+              xs="12"
+              md={{ offset: 2, span: 6 }}
+              className="contact-inputs text-right"
+            >
               <Form>
                 <Row>
                   <Col xs="12" md="6">
@@ -131,25 +126,24 @@ const ContactUs = () => {
                         className="custom-input"
                         type="text"
                         placeholder="Joe"
-                        onChange={ (e) => {
+                        value={data.first_name}
+                        onChange={(e) => {
                           setdata({ ...data, first_name: e.target.value });
-                        } }
+                        }}
                       />
                     </Form.Group>
                   </Col>
                   <Col xs="12" md="6">
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicPassword"
-                    >
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
                       <Form.Label>Last Name</Form.Label>
                       <Form.Control
                         className="custom-input"
                         type="text"
                         placeholder="Doe"
-                        onChange={ (e) => {
+                        value={data.last_name}
+                        onChange={(e) => {
                           setdata({ ...data, last_name: e.target.value });
-                        } }
+                        }}
                       />
                     </Form.Group>
                   </Col>
@@ -160,26 +154,22 @@ const ContactUs = () => {
                         className="custom-input"
                         type="email"
                         placeholder="Enter email"
-                        onChange={ (e) => {
+                        onChange={(e) => {
                           setdata({ ...data, email: e.target.value });
-                        } }
+                        }}
                       />
                     </Form.Group>
                   </Col>
                   <Col xs="12" md="6">
-                    <Form.Group
-                      className="mb-3"
-                      controlId="formBasicPassword"
-                    >
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
                       <Form.Label>Phone</Form.Label>
                       <Form.Control
                         className="custom-input"
                         type="number"
                         placeholder="0000000"
-                        onChange={ (e) => {
-
+                        onChange={(e) => {
                           setdata({ ...data, mobile: e.target.value });
-                        } }
+                        }}
                       />
                     </Form.Group>
                   </Col>
@@ -192,12 +182,12 @@ const ContactUs = () => {
                             type="checkbox"
                             label="Schedule a Meeting"
                             value="Schedule a Meeting"
-                            onClick={ (e) => {
+                            onClick={(e) => {
                               setdata({
                                 ...data,
                                 way_to_use: e.target.value,
                               });
-                            } }
+                            }}
                           />
                         </Col>
                         <Col xs="12" sm="6" md="4">
@@ -205,12 +195,12 @@ const ContactUs = () => {
                             type="checkbox"
                             label="Schedule a Call"
                             value="Schedule a Call"
-                            onClick={ (e) => {
+                            onClick={(e) => {
                               setdata({
                                 ...data,
                                 way_to_use: e.target.value,
                               });
-                            } }
+                            }}
                           />
                         </Col>
                         <Col xs="12" sm="6" md="4">
@@ -218,12 +208,12 @@ const ContactUs = () => {
                             type="checkbox"
                             label="Come to the Office"
                             value="Come to the Office"
-                            onClick={ (e) => {
+                            onClick={(e) => {
                               setdata({
                                 ...data,
                                 way_to_use: e.target.value,
                               });
-                            } }
+                            }}
                           />
                         </Col>
                       </Row>
@@ -237,11 +227,11 @@ const ContactUs = () => {
                       <Form.Label>Message</Form.Label>
                       <Form.Control
                         as="textarea"
-                        rows={ 3 }
+                        rows={3}
                         className="custom-input"
-                        onChange={ (e) => {
+                        onChange={(e) => {
                           setdata({ ...data, message: e.target.value });
-                        } }
+                        }}
                       />
                     </Form.Group>
                   </Col>
@@ -250,9 +240,7 @@ const ContactUs = () => {
                       className="d-flex"
                       variant="primary"
                       type="submit"
-                      onClick={ (e) => {
-
-                        postquery();
+                      onClick={(e) => {
                         setSpin(
                           <Spinner
                             as="span"
@@ -265,23 +253,27 @@ const ContactUs = () => {
                         e.preventDefault();
                         setTimeout(() => {
                           setSpin("Send Message");
-                          setAlert("Message Sent Successfully...");
+                          postquery();
                         }, 2000);
                         setTimeout(() => {
-                          setAlert("");
+                          setdata({ ...data, first_name: " " });
+                          setdata({ ...data, last_name: " " });
+                          setdata({ ...data, mobile: " " });
+                          setdata({ ...data, email: " " });
+                          setdata({ ...data, way_to_use: " " });
+                          setdata({ ...data, message: " " });
                         }, 3000);
-                      } }
+                      }}
                     >
-                      { spin }
+                      {spin}
                     </Button>
-                    <small className="text-success mt-2"> { alert } </small>
+                    <small className="text-success mt-2"> {alert} </small>
                   </Col>
                 </Row>
               </Form>
             </Col>
           </Row>
         </Container>
-
       </div>
     </Scroll.Element>
   );
