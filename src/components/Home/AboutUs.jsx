986a80e62bc6badfc68e6af0dc1b692cce0f2248
munib <munibkhatri123@gmail.com>
2022-02-09 13:39:51 +0500

    update about us section

--- src/components/Home/AboutUs.jsx
+++ src/components/Home/AboutUs.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   async function fetchaboutdata() {
     const response = await Action.get("/about", {});
-    if (response.data.success ===true) {
+    if (response.data.success === true) {
       setabout(response.data.data[0]);
     } else {
       setabout([]);
@@ -20,21 +20,18 @@ const Navbar = () => {
   }
   async function fetchservicedata() {
     const res = await Action.get("/Service", {});
-    console.log(res);
-    if (res.data.success ===true) {
-      console.log(res.data.data);
+
+    if (res.data.success === true) {
       setservice(res.data.data);
     } else {
       setservice([]);
     }
   }
-
-  useEffect(async () => {
+  console.log(service[0]);
+  useEffect(() => {
     fetchaboutdata();
     fetchservicedata();
   }, []);
-console.log("====================================");
-  console.log(service);
 
   return (
     <Container id="about-us">
@@ -64,44 +61,42 @@ console.log("====================================");
           </Col>
           <Col xs="12" md="6">
             <Row className="text-center about-cards justify-content-center">
-              <Col xs="5" md="5" className=" each-card py-4 ">
+              {
+                service.map((item,index)=>{
+                  if(index==4){
+                    return null;
+                  }
+                return (
+                  <Col xs="5" md="5" className=" each-card py-4 ">
                 <img
-                  src={baseURL + service[0].image}
+                  src={ baseURL +item.image}
+                  alt="service"
                   height="30%"
                   width="30%"
                 />
-                <h2>{service[0].heading} </h2>
+                <h2>{item.heading} </h2>
                 {/* <p> { about.txt1 }</p> */}
+              </Col>)
+              })
+
+              }
+
+              {/* <Col xs="5" md="5" className=" each-card py-4 ">
+                <img src={baseURL + service.image} height="30%" width="30%" />
+                 <h2>100 </h2> 
+                <p> {service.heading}</p>
               </Col>
 
               <Col xs="5" md="5" className=" each-card py-4 ">
-                <img
-                  src={baseURL + service[1].image}
-                  height="30%"
-                  width="30%"
-                />
-                {/* <h2>100 </h2> */}
-                <p> {service[1].heading}</p>
-              </Col>
-
-              <Col xs="5" md="5" className=" each-card py-4 ">
-                <img
-                  src={baseURL + service[2].image}
-                  height="30%"
-                  width="30%"
-                />
-                {/* <h2>100 </h2> */}
-                <p> {service[2].heading}</p>
+                <img src={baseURL + service.image} height="30%" width="30%" />
+                <h2>100 </h2> 
+                <p> {service.heading}</p>
               </Col>
               <Col xs="5" md="5" className=" each-card py-4 ">
-                <img
-                  src={baseURL + service[3].image}
-                  height="30%"
-                  width="30%"
-                />
-                {/* <h2>100 </h2> */}
-                <p> {service[3].heading}</p>
-              </Col>
+                <img src={baseURL + service.image} height="30%" width="30%" />
+                 <h2>100 </h2> 
+                <p> {service.heading}</p>
+              </Col> */}
             </Row>
           </Col>
         </Row>
--- src/middleware/BaseURL.js
+++ src/middleware/BaseURL.js
@@ -1,4 +1,5 @@
-const baseURL = "http://localhost:4000/image/"
+// const baseURL = "http://localhost:4000/image/"
+const baseURL= `https://nanyapp2.herokuapp.com/image/`
 // const baseURL= 'http://3.94.191.228:4000';
 // const baseURL = `https://evening-inlet-11817.herokuapp.com/`;
 export default baseURL
