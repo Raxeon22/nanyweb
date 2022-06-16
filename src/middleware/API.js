ae3f530334569bdd29a65465ae39cdd09827fd6f
huzaifa2213 <huzaifa.kamran1322@gmail.com>
2022-06-16 17:15:41 +0500

    api url changed

--- src/middleware/API.js
+++ src/middleware/API.js
@@ -4,5 +4,5 @@ import axios from 'axios'
 export default axios.create({
     // baseURL: `http://localhost:4000`
     // baseURL:'http://3.94.191.228:4000'
-    baseURL: `https://nanybackendforadmin.herokuapp.com`
+    baseURL: `https://nanny-backend-app.herokuapp.com/`
 })
--- src/middleware/BaseURL.js
+++ src/middleware/BaseURL.js
@@ -1,5 +1,5 @@
 //const baseURL = "http://localhost:4000/uploads/"
-const baseURL = `https://nanybackendforadmin.herokuapp.com/image/`
+const baseURL = `https://nanny-backend-app.herokuapp.com/image/`
 // const baseURL= 'http://3.94.191.228:4000';
 // const baseURL = `https://evening-inlet-11817.herokuapp.com/`;
 export default baseURL
